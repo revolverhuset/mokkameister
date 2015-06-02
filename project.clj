e051be0a6716463c6374db7a8158c4e7500c41b6
Stian Eikeland <stian@eikeland.se>
2015-06-02 18:44:48 +0200

    Handle /coffee <min> and /coffee instant

--- project.clj
+++ project.clj
@@ -3,13 +3,15 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[cheshire "5.3.1"]
+                 [clj-http "1.1.2"]
                  [compojure "1.1.8"]
-                 [ring/ring-jetty-adapter "1.2.2"]
-                 [ring/ring-devel "1.2.2"]
                  [environ "1.0.0"]
-                 [cheshire "5.3.1"]
-                 [clj-http "1.1.2"]]
+                 [org.clojure/clojure "1.6.0"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [ring/ring-defaults "0.1.5"]
+                 [ring/ring-devel "1.2.2"]
+                 [ring/ring-jetty-adapter "1.2.2"]]
   :plugins [[lein-environ "1.0.0"]]
   :hooks [environ.leiningen.hooks]
   :min-lein-version "2.0.0"
--- src/mokkameister/web.clj
+++ src/mokkameister/web.clj
@@ -1,12 +1,43 @@
 (ns mokkameister.web
   (:require [compojure.core :refer [defroutes GET PUT POST DELETE ANY]]
             [ring.adapter.jetty :as jetty]
-            [ring.middleware.stacktrace :as trace]
+            [ring.middleware.stacktrace :refer [wrap-stacktrace]]
+            [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
             [environ.core :refer [env]]
             [clojure.pprint :refer [pprint]]
-            [cheshire.core :as json]))
+            [cheshire.core :as json]
+            [mokkameister.slack :as slack]
+            [mokkameister.util :refer [parse-int]]))
+
+(defn- coffee-message-starting [user minutes]
+  (format "God nyhendnad folket! %s starta nett kaffitraktaren, kaffi om %d minuttar!" user minutes))
+
+(defn- coffee-message-finished [user]
+  (format "Det er kaffi å få på kjøken! @%s" user))
+
+(defn coffee-message-instant [user]
+  (format "Den sleipe robusta-knaskaren %s har lagt seg ein snar-kaffi :/" user))
+
+(defn handle-instant-coffee [params]
+  (slack/notify (coffee-message-instant (:user_name params))))
+
+(defn handle-regular-coffee [params]
+  (let [time (or (parse-int (:text params)) 5)
+        time-ms (* time 1000 60)
+        user (:user_name params)]
+    (slack/notify (coffee-message-starting user time))
+    (slack/delayed-notify time-ms (coffee-message-finished user))))
+
+(defn handle-slack-coffee [params]
+  (if (= (:text params) "instant")
+    (handle-instant-coffee params)
+    (handle-regular-coffee params))
+  {:status 200
+   :body ""})
 
 (defroutes app
+  (POST "/slack-coffee" req
+        (handle-slack-coffee (:params req)))
   (GET "/" []
        {:status 200
         :headers {"Content-Type" "text/plain"}
@@ -14,7 +45,8 @@
 
 (defn wrap-middlewares [app]
   (-> app
-      trace/wrap-stacktrace))
+      wrap-stacktrace
+      (wrap-defaults api-defaults)))
 
 (defn -main [& [port]]
   (let [port (Integer. (or port (env :port) 5000))]
