8f60ba912d14cf7682eb76c23179bec53750121c
Stian Eikeland <stian@eikeland.se>
2015-06-02 18:42:40 +0200

    Cleaned up slack notify

--- src/mokkameister/slack.clj
+++ src/mokkameister/slack.clj
@@ -1,13 +1,26 @@
 (ns mokkameister.slack
   (:require [clj-http.client :as client]
-            [environ.core :refer [env]]))
+            [environ.core :refer [env]]
+            [clojure.core.async :as a :refer [go timeout <!]]))
 
-(def slack-url (env :slack-url))
+(def slack-url (or (env :slack-url)
+                   (throw (Exception. "Missing SLACK_URL environment variable"))))
 
-(defn notify [message]
+(defn notify
+  "Send message to slack. Optional arguments: channel, emoji, username."
+  [message & {:keys [channel username emoji]
+              :or {channel  "#testroompleaseignore"
+                   emoji    ":coffee:"
+                   username "Mokkameister 9000"}}]
   (let [payload {:text message
-                 :username "Mokkameister 9000"
-                 :icon_emoji ":coffee:"
-                 :channel "#testroompleaseignore"}]
-    (client/post slack-url {:form-params payload
-                            :content-type :json})))
+                 :username username
+                 :icon_emoji emoji
+                 :channel channel}]
+    (client/post slack-url {:content-type :json
+                            :form-params payload})))
+
+(defn delayed-notify
+  "Send a delayed (millisec) message to slack."
+  [delay-ms & args]
+  (go (<! (timeout delay-ms))
+      (apply notify args)))
