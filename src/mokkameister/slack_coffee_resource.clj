67b9cecd6837c84ba1754d654adee316dd7985e7
Stian Eikeland <stian@eikeland.se>
2015-06-06 12:04:45 +0200

    Refactor

--- src/mokkameister/slack_coffee_resource.clj
+++ src/mokkameister/slack_coffee_resource.clj
@@ -4,37 +4,38 @@
             [mokkameister.system :refer [system]]
             [mokkameister.util :refer [parse-int]]))
 
-(defn- coffee-message-starting [user minutes]
-  (format "God nyhendnad folket! %s starta nett kaffitraktaren, kaffi om %d minuttar!" user minutes))
+(defn- coffee-message-starting [{:keys [slack-user brew-time]}]
+  (format "God nyhendnad folket! %s starta nett kaffitraktaren, kaffi om %d minuttar!"
+          slack-user brew-time))
 
-(defn- coffee-message-finished [user]
-  (format "Det er kaffi å få på kjøken! @%s" user))
+(defn- coffee-message-finished [{:keys [slack-user]}]
+  (format "Det er kaffi å få på kjøken! @%s" slack-user))
 
-(defn- coffee-message-instant [user]
-  (format "Den sleipe robusta-knaskaren %s har lagt seg ein snar-kaffi :/" user))
+(defn- coffee-message-instant [{:keys [slack-user]}]
+  (format "Den sleipe robusta-knaskaren %s har lagt seg ein snar-kaffi :/" slack-user))
 
 
 (defmulti handle-slack-coffee :coffee-type)
 
-(defmethod handle-slack-coffee :instant [{:keys [user channel]}]
-  (let [msg (coffee-message-instant user)]
+(defmethod handle-slack-coffee :instant [{:keys [channel] :as event}]
+  (let [msg (coffee-message-instant event)]
     (slack/notify msg :channel channel)))
 
-(defmethod handle-slack-coffee :regular [{:keys [user channel time time-ms]}]
-  (let [now-msg   (coffee-message-starting user time)
-        later-msg (coffee-message-finished user)]
+(defmethod handle-slack-coffee :regular [{:keys [channel time-ms] :as event}]
+  (let [now-msg   (coffee-message-starting event)
+        later-msg (coffee-message-finished event)]
     (slack/notify now-msg :channel channel)
     (slack/delayed-notify time-ms later-msg :channel channel)))
 
 
 (defn parse-slack-coffee-event [{:keys [channel_id text user_name]}]
-  (let [event {:channel channel_id
-               :user    user_name}]
+  (let [event {:channel    channel_id
+               :slack-user user_name}]
     (if (= text "instant")
-      (assoc event :coffee-type :instant)
+      (assoc event :coffee-type :instant, :brew-time 0)
       (let [time    (or (parse-int text) 5)
             time-ms (* time 1000 60)]
-        (assoc event :coffee-type :regular, :time time, :time-ms time-ms)))))
+        (assoc event :coffee-type :regular, :brew-time time, :time-ms time-ms)))))
 
 (defn valid-slack-token? [ctx]
   (let [token (get-in ctx [:request :params :token])]
