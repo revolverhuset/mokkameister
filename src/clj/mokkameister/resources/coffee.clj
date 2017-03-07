6f44c104a7e4e48368693153064358aee5445687
Stian Eikeland <stian@eikeland.se>
2017-03-07 19:34:10 +0100

    Jodatime json generator

--- src/clj/mokkameister/resources/coffee.clj
+++ src/clj/mokkameister/resources/coffee.clj
@@ -1,11 +1,25 @@
 (ns mokkameister.resources.coffee
   "API for web page (status / stats)"
   (:require [cheshire.core :as json]
+            [cheshire.generate :as generate]
+            [clj-time.core :as t]
+            [clj-time.coerce :as c]
             [liberator.core :refer [defresource]]
-            [mokkameister.db.persistence :refer [brew-stats latest-brews month-stats]]))
+            [mokkameister.db.persistence :refer [brew-stats find-last-regular-coffee month-stats]]))
+
+(defn add-common-json-encoders!*
+  "Non-memoize version of add-common-json-encoders!"
+  []
+  (generate/add-encoder
+    org.joda.time.DateTime
+    (fn [data jsonGenerator]
+      (.writeString jsonGenerator (c/to-string data)))))
+
+(add-common-json-encoders!*)
 
 (defn- status [_]
-  {:latest (latest-brews)})
+  (let [[latest] (find-last-regular-coffee)]
+    {:latest latest}))
 
 (defn- stats [_]
   {:brews (brew-stats)
