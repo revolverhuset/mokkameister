5df4ad6406dedb11d6f3d49dbd99a2523822b729
Stian Eikeland <stian@eikeland.se>
2017-03-07 19:35:12 +0100

    Render new brew status

--- src/cljs/mokkameister/core.cljs
+++ src/cljs/mokkameister/core.cljs
@@ -1,6 +1,8 @@
 (ns mokkameister.core
-  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require-macros [cljs.core.async.macros :refer [go go-loop]])
   (:require [mokkameister.random :refer [rand-nth-weighted]]
+            [mokkameister.brew-status :refer [brew-status]]
+            [mokkameister.loading :refer [loading-gif]]
             [reagent.core :as reagent :refer [atom]]
             [ajax.core :refer [GET POST]]
             [cljs.core.async :as async :refer [timeout <!]]
@@ -9,8 +11,9 @@
 (enable-console-print!)
 
 (defonce state (atom {}))
+(defonce timer (atom 0))
 
-(add-watch state :listener
+#_(add-watch state :listener
            (fn [key atom old new]
              (prn "New state:", new)))
 
@@ -24,33 +27,10 @@
         :keywords? true
         :handler (fn [data] (swap! state #(assoc % :stats data)))}))
 
-(defn friendly-time-ago [time]
-  (-> (js/moment time)
-      (.locale "nn") ;; Nynorsk!
-      (.fromNow)))
-
-(defn- play-siren! []
-  (let [audio (js/Audio. "/alarm.mp3")]
-    (.play audio)))
-
-(defn- loading-gif []
-  [:div.loading
-   [:img {:src "img/loading.gif"}]])
-
-(defn last-brew []
-  (let [timer (atom 0)]
-    (fn []
-      (go (<! (timeout 5000))
-          (swap! timer inc))
-      @timer ;; Ugly... deref atom to re-eval component.. :(
-      (if-let [time (get-in @state [:latest :regular :created])]
-        [:div (str "Sist brygg blei laga " (friendly-time-ago time) ".")]
-        (loading-gif)))))
-
-(defn alarm []
-  (when-let [message (:alarm @state)]
-    [:div.alert.alert-danger.alarm {:role "alert"}
-     [:strong.blink "ALARM! ALARM! "] message]))
+(defn brew-status-on-timer []
+  (let [brew (get-in @state [:latest])
+        ticks @timer] ; deref timer forces eval
+    (brew-status brew)))
 
 (defn stats []
   (if-let [stats (get-in @state [:stats :brews :regular])]
@@ -95,27 +75,26 @@
                         (chart chart-data)
                         (loading-gif))]]))
 
-(defn trigger-alarm! [message]
-  (swap! state #(assoc % :alarm message))
-  (fetch-data!)
-  (play-siren!)
-  (go (<! (timeout (* 2 60 1000)))
-      (swap! state #(dissoc % :alarm))))
-
 (defn subscribe-pusher! []
   (when-not (:pusher @state)
     (let [settings (js-obj "cluster" "eu",
                            "encrypted" true)
           socket (new js/Pusher "74106d5bea6fc6c2ed86" settings)
           channel (.subscribe socket "coffee")]
-      (.bind channel "coffee" trigger-alarm!)
+      (.bind channel "brewing" (fn [_] (fetch-data!)))
       (swap! state #(assoc % :pusher socket)))))
 
+(defn start-timer! []
+  (go-loop []
+    (<! (timeout 5000))
+    (swap! timer inc)
+    (recur)))
+
 (do
-  (reagent/render-component [last-brew] (.getElementById js/document "last-brew"))
+  (reagent/render-component [brew-status-on-timer] (.getElementById js/document "brew-status"))
   (reagent/render-component [stats] (.getElementById js/document "stats"))
-  (reagent/render-component [alarm] (.getElementById js/document "alarm"))
   (reagent/render-component [chart-panel] (.getElementById js/document "chart-panel"))
   (fetch-data!)
   (subscribe-pusher!)
+  (start-timer!)
   (println "Running!"))
