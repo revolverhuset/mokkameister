7abe7fc58bf4553dc6e78f8231247a6bc31d2bbc
Stian Eikeland <stian@eikeland.se>
2017-03-16 11:45:57 +0100

    Keyword those trains

--- src/clj/mokkameister/train.clj
+++ src/clj/mokkameister/train.clj
@@ -1,59 +1,65 @@
 (ns mokkameister.train
   (:require [mokkameister.random :refer [rand-nth-weighted rand-sexp]]))
 
-(def ^:private locomotives {":steam_locomotive:" 9
-                            ":bybane2:" 3
-                            ":bullettrain_front:" 3
-                            ":train:" 1
-                            ":horse:" 2
-                            ":unicorn_face:" 1
-                            ":boar:" 1
-                            ":tractor:" 1})
+(def ^:private locomotives {:steam_locomotive 9
+                            :bybane2 3
+                            :bullettrain_front 3
+                            :train 1
+                            :horse 2
+                            :unicorn_face 1
+                            :boar 1
+                            :tractor 1})
 
-(def ^:private train-endings {":dash:" 8
-                              "" 2
-                              ":afterburner:" 2
-                              ":sparkles:" 2
-                              ":exclamation:" 1})
+(def ^:private train-endings {:dash 8
+                              :afterburner 2
+                              :sparkles 2
+                              :exclamation 1
+                              :rainbow 1})
 
 (def ^:private num-wagons 7)
 
+(defn- key->emoji [s]
+  (str s ":"))
+
 (defn- train [& wagons]
-  (let [front  (rand-nth-weighted locomotives)
-        back   (rand-nth-weighted train-endings)
-        middle (apply str (take num-wagons (cycle wagons)))]
+  (let [front  (key->emoji (rand-nth-weighted locomotives))
+        back   (key->emoji (rand-nth-weighted train-endings))
+        middle (->> (map key->emoji wagons)
+                    (cycle)
+                    (take num-wagons)
+                    (apply str))]
     (str front middle back)))
 
-(def ^:private coffee-train (partial train ":coffee:"))
+(def ^:private coffee-train (partial train :coffee))
 
 (defn rand-train []
   (rand-sexp
    (coffee-train)
    (coffee-train)
-   (coffee-train ":heart:")
-   (coffee-train ":heart:")
-   (coffee-train ":heart:")
-   (coffee-train ":syringe:")
-   (coffee-train ":mushroom:")
-   (coffee-train ":coffeepot:")
-   (coffee-train ":coffeealarm:")
-   (coffee-train ":hocho:")
-   (coffee-train ":wrench:")
-   (coffee-train ":tada:")
-   (coffee-train ":bacon:")
-   (coffee-train ":nuclear:")
-   (coffee-train ":timwendelboe:")
-   (coffee-train ":jan-richter:")
-   (coffee-train ":scream:")
-   (coffee-train ":heavy_heart_exclamation_mark_ornament:")
-   (coffee-train ":badger:" ":mushroom:")
-   (coffee-train ":coffeepot:" ":heart:")
-   (coffee-train ":simply_overcaffeinated:" ":coffeespin:")
-   (coffee-train ":smoking:")
-   (train ":coffeealarm:" ":coffeealarm:")
-   (train ":nespresso:" ":ali:")
-   (train ":ali:" ":gruff:")
-   (train ":syringe:" ":hocho:")
+   (coffee-train :heart)
+   (coffee-train :heart)
+   (coffee-train :heart)
+   (coffee-train :syringe)
+   (coffee-train :mushroom)
+   (coffee-train :coffeepot)
+   (coffee-train :coffeealarm)
+   (coffee-train :hocho)
+   (coffee-train :wrench)
+   (coffee-train :tada)
+   (coffee-train :bacon)
+   (coffee-train :nuclear)
+   (coffee-train :timwendelboe)
+   (coffee-train :jan-richter)
+   (coffee-train :scream)
+   (coffee-train :smoking)
+   (coffee-train :heavy_heart_exclamation_mark_ornament)
+   (coffee-train :badger :mushroom)
+   (coffee-train :coffeepot :heart)
+   (coffee-train :simply_overcaffeinated :coffeespin)
+   (train :coffeealarm)
+   (train :nespresso :ali)
+   (train :ali :gruff)
+   (train :syringe :hocho)
    ":steam_locomotive::coffee::coffee::running::running::running::dash:"
    "All aboard the coffee train\n:steam_locomotive::coffee::coffee::running::running::running::dash:"
    "Det er kaffi å få på kjøken!"
