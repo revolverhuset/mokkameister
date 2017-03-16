6f1140c7481b1ca77f17185fa4e7b7be11204844
Magnus Hoff <maghoff@gmail.com>
2017-03-16 10:48:46 +0100

    Sunshine, lollipops and rainbows

--- src/clj/mokkameister/train.clj
+++ src/clj/mokkameister/train.clj
@@ -48,4 +48,5 @@
    "Det er kaffi å få på kjøken!"
    "KEEP CALM THE COFFEE IS READY!"
    "M-m-m-m-m-microdosing! :syringe::syringe::syringe::exclamation:"
-   "PLEASE PROCEED IN ORDERLY FASHION TO RECEIVE COFFEE :syringe:"))
+   "PLEASE PROCEED IN ORDERLY FASHION TO RECEIVE COFFEE :syringe:"
+   ":steam_locomotive::sun_with_face::lollipop::sun_with_face::lollipop::sun_with_face::lollipop::sun_with_face::rainbow:"))
