7100ac56a5f6a01fdf789dcdf93ec92baea2815c
Stian Eikeland <stian@eikeland.se>
2018-01-25 00:10:14 +0100

    Test-endpoint for new coffee button

--- src/clj/mokkameister/resources/button.clj
+++ src/clj/mokkameister/resources/button.clj
@@ -1,13 +1,14 @@
 (ns mokkameister.resources.button
   (:require [liberator.core :refer [defresource]]
             [mokkameister.brew :refer [start-brewing!]]
+            [mokkameister.slack :refer [notify]]
             [mokkameister.system :refer [system]]))
 
 (defn- valid-button-token? [ctx]
   (let [token (get-in ctx [:request :params :secret])]
     (= token (system :button-token))))
 
-(defn- handle-button-post [ctx]
+(defn- handle-old-button-post [ctx]
   (let [brew {:channel "#penthouse"
               :slack-user "nokon"
               :brew-time 5
@@ -15,8 +16,15 @@
     (start-brewing! brew))
   "OK")
 
+(defn- handle-button-get [ctx]
+  ;; New button uses GET, seems like SendToHTTP
+  ;; command is very basic :(
+  (notify "Kaffi!" :channel "#testroompleaseignore")
+  "OK")
+
 (defresource coffee-button
   :available-media-types ["text/plain"]
   :allowed-methods [:post]
   :authorized? valid-button-token?
-  :post! handle-button-post)
+  :post! handle-old-button-post
+  :handle-ok handle-button-get)
