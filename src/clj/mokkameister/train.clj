3a6b574c605c9b22ff5ceb6bda652823cf44a470
Stian Eikeland <stian@eikeland.se>
2018-06-25 11:37:07 +0200

    Forgot to remove afterburner, coffee overspeed.

--- src/clj/mokkameister/train.clj
+++ src/clj/mokkameister/train.clj
@@ -79,7 +79,7 @@
    (train-custom :coffee :coffee :running :running :running :dash)
    (train-custom :sun_with_face :lollipop :sun_with_face :lollipop
                  :sun_with_face :lollipop :sun_with_face :rainbow)
-   ":bullettrain_front::coffee::scream_cat::coffee::scream_cat::afterburner::dash:"
+   ":bullettrain_front::coffee::scream_cat::coffee::scream_cat::dash:"
    ":tractor::cow2::cow::cow2::cow::cow2::cow::cow2::dash:"
    (str "All aboard the coffee train!\n"
         (train-custom :coffee :coffee :running :running :running :dash))
